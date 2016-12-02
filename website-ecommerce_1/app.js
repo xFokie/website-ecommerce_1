5e9e942bfbc9c5fb573d6c3319b4defb1777b85b
Ftujio <svetlin954@abv.bg>
2016-12-02 18:56:47 +0200

    Loaded "express-validator"

--- website-ecommerce_1/app.js
+++ website-ecommerce_1/app.js
@@ -31,6 +31,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 
 app.use(session({
